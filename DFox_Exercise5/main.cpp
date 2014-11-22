57ad5b10c0b094a56928e9a31f7b39283d17dbf5
Derrick Fox <HomeFolder@Derricks-Mac-mini.local>
2014-11-22 13:58:15 -0500

    Initial Commit

---
+++ DFox_Exercise5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DFox_Exercise5
+//
+//  Created by Derrick Fox on 11/22/14.
+//  Copyright (c) 2014 Derrick Fox. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
